a571e3fdb16a2c17633803a2508bdaf4dba32b34
devel@traal.ch <devel@traal.ch>
2020-01-05 16:36:40 +0100

    feat: Add fields password and confirmPassword to the class.

--- src/main/java/com/example/demo/model/requests/CreateUserRequest.java
+++ src/main/java/com/example/demo/model/requests/CreateUserRequest.java
@@ -2,11 +2,32 @@ package com.example.demo.model.requests;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * DTO CreateUserRequest
+ * 
+ * @author traal-devel
+ */
 public class CreateUserRequest {
 
+  
+  /* member variables */
 	@JsonProperty
 	private String username;
+	
+	@JsonProperty
+	private String password;
+	
+	@JsonProperty
+	private String confirmPassword;
+	
 
+	/* constructors */
+	public CreateUserRequest() {
+	  super();
+	}
+	
+	
+	/* methods */
 	public String getUsername() {
 		return username;
 	}
@@ -14,4 +35,22 @@ public class CreateUserRequest {
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getConfirmPassword() {
+    return confirmPassword;
+  }
+
+  public void setConfirmPassword(String confirmPassword) {
+    this.confirmPassword = confirmPassword;
+  }
+	
+	
 }
