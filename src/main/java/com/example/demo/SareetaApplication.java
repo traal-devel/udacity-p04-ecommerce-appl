d56afd51771c2268c7c8dd164f5d83ed6722ae15
devel@traal.ch <devel@traal.ch>
2020-01-05 16:43:24 +0100

    feat: Add bcrypt password encoder to spring main configuration.

--- src/main/java/com/example/demo/SareetaApplication.java
+++ src/main/java/com/example/demo/SareetaApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  * eCommerce Spring Boot Application starting point. 
@@ -37,6 +39,11 @@ public class SareetaApplication {
 	    String[] args
 	) {
 		SpringApplication.run(SareetaApplication.class, args);
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+	  return new BCryptPasswordEncoder();
 	}
 
 }
