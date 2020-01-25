3417d1466e1e1df68e2df2fa26e547657410dc11
devel@traal.ch <devel@traal.ch>
2020-01-25 08:54:35 +0100

    refactor: Change name HEADER_STRING to HEADER_AUTHORIZATION.

--- src/main/java/com/example/demo/def/SecurityConstants.java
+++ src/main/java/com/example/demo/def/SecurityConstants.java
@@ -9,10 +9,10 @@ public class SecurityConstants {
 
   
   /* constatns */
-  public static final long   EXPIRATION_TIME  = 864_000_000; // 10 days
-  public static final String HEADER_STRING    = "Authorization";
+  public static final long   EXPIRATION_TIME  = 864_000_000;            // 10 days
+  public static final String HEADER_AUTHORIZATION    = "Authorization";
   public static final String TOKEN_PREFIX     = "Bearer ";
-  public static final String SECRET           = "oursecretkey";
+  public static final String SECRET           = "oursecretkey";         // Read this from a config-server
   public static final String SIGN_UP_URL      = "/api/user/create";
   
   
