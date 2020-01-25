268daeef25a3852e8c66c57c57f03fd29487d0e9
devel@traal.ch <devel@traal.ch>
2020-01-25 12:47:00 +0100

    feat: Implement login_authorized and login_unauthorized.

--- src/test/java/com/example/demo/controllers/SecurityTest.java
+++ src/test/java/com/example/demo/controllers/SecurityTest.java
@@ -14,6 +14,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.example.demo.model.requests.CreateUserRequest;
 import com.example.demo.model.requests.LoginRequest;
 
 @RunWith(SpringRunner.class)
@@ -22,34 +23,68 @@ import com.example.demo.model.requests.LoginRequest;
 )
 @AutoConfigureMockMvc
 @AutoConfigureJsonTesters
-public class SareetaApplicationTest {
+public class SecurityTest {
 
   
   /* member variables */
   @Autowired
-  private MockMvc                       mvc;
+  private MockMvc                             mvc;
   
   @Autowired
-  private JacksonTester<LoginRequest>   jsonReview;
+  private JacksonTester<LoginRequest>         jsonLoginRequest;
+  
+  @Autowired
+  private JacksonTester<CreateUserRequest>    jsonCreateUserRequest;
   
   /* constructors */
 
   
   /* methods */
   @Test
-  public void login() throws Exception {
+  public void login_unauthorized() throws Exception {
     LoginRequest request = new LoginRequest();
     request.setUsername("admin");
     request.setPassword("test");
     
     mvc.perform(
           post("/login")
-            .content(jsonReview.write(request).getJson())
+            .content(jsonLoginRequest.write(request).getJson())
+            .contentType(MediaType.APPLICATION_JSON_UTF8)
+            .accept(MediaType.APPLICATION_JSON_UTF8)
+        )
+        .andExpect(status().isUnauthorized());
+    
+  }
+  
+  @Test
+  public void login_authorized() throws Exception {
+    String username = "johndoe";
+    String password = "test1234#";
+    CreateUserRequest request = new CreateUserRequest();
+    request.setUsername(username);
+    request.setPassword(password);
+    request.setConfirmPassword(password);
+    
+    mvc.perform(
+          post("/api/user/create")
+            .content(jsonCreateUserRequest.write(request).getJson())
             .contentType(MediaType.APPLICATION_JSON_UTF8)
             .accept(MediaType.APPLICATION_JSON_UTF8)
         )
         .andExpect(status().isOk());
     
+    
+    LoginRequest loginRequest = new LoginRequest();
+    loginRequest.setUsername(username);
+    loginRequest.setPassword(password);
+    
+    mvc.perform(
+          post("/login")
+            .content(jsonLoginRequest.write(loginRequest).getJson())
+            .contentType(MediaType.APPLICATION_JSON_UTF8)
+            .accept(MediaType.APPLICATION_JSON_UTF8)
+        )
+        .andExpect(status().isOk());
   }
 
 }
